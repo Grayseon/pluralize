f42d4e582edb0f3af6bbd868d86c11c388a256ac
Grayseon <grayseon@gmail.com>
2025-07-16 11:00:37 -0500

    add weird pluralization rules

--- index.js
+++ index.js
@@ -14,17 +14,19 @@ export const s = "__@@PLURALIZE"
 export function pluralize(strings, ...values) {
   if (!values.includes(s)) throw new TypeError("Pluralization is unnecessary; it was never called.")
 
-  //const stitched = stitch(strings, values)
-
   let pluralized = true
-  const pluralizedValues = values.map((value) => {
+  const pluralizedValues = values.map((value, i) => {
     const parsedValue = parseFloat(value)
     if (parsedValue === 1) {
       pluralized = false
     } else if (value === s) {
       const oldPluralized = pluralized
       pluralized = true
-      if (oldPluralized) { return "s" } else { return "" }
+      if (oldPluralized) {
+        const string = strings[i].toString()
+        if(string.match(/[c|t|p]h$/)) return "es"
+        return "s"
+      } else { return "" }
     }
     return value
   })
--- test.js
+++ test.js
@@ -2,5 +2,6 @@ import { pluralize, s } from "./index.js"
 
 const notifications = 1
 const antelopes = 5
+const lunches = 2
 
-console.log(pluralize`you have ${notifications} notification${s} and ${antelopes} antelope${s}.`)
+console.log(pluralize`you have ${notifications} notification${s}, ${antelopes} antelope${s}, and ${lunches} lunch${s}.`)
